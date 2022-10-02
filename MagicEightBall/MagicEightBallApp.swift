c28291f9d6179227ff38216f4d5cc5db90d4cc9f
Charles Joma <almprincecharles@yahoo.com>
2022-10-02 14:45:39 +1100

    Initial Commit

---
+++ MagicEightBall/MagicEightBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicEightBallApp.swift
+//  MagicEightBall
+//
+//  Created by Charles Joma on 2/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MagicEightBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
